111d1dacdc30d1ff0a54e8209d19414aaedc0592
one-atom <519001901@qq.com>
2017-03-05 00:23:21 +0800

    create an empty window

--- Gomoku.py
+++ Gomoku.py
@@ -7,7 +7,7 @@ class Gomoku():
 	def __init__(self):
 		pygame.init()  # init a intance of pygame
 		self.going = True # this is a signal of 
-		self.screen = pygame.display.set_mode((800, 600))
+		self.screen = pygame.display.set_mode((800, 600)) # set up the initial window
 	
 	def loop(self):
 		while self.going:
@@ -18,6 +18,8 @@ class Gomoku():
 		for e in pygame.event.get():
 			if e.type == pygame.QUIT:
 				self.going = False
+
+	def draw
 
 if __name__ == '__main__':
 	game = Gomoku()
