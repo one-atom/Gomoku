efbfe02a4d8c4ba2d92098051c915467f6fb7103
one-atom <519001901@qq.com>
2017-03-05 00:20:27 +0800

    create an empty window

---
+++ Gomoku.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import pygame
+import platform
+
+class Gomoku():
+
+	def __init__(self):
+		pygame.init()  # init a intance of pygame
+		self.going = True # this is a signal of 
+		self.screen = pygame.display.set_mode((800, 600))
+	
+	def loop(self):
+		while self.going:
+			self.update()
+		pygame.quit()
+
+	def update(self):
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				self.going = False
+
+if __name__ == '__main__':
+	game = Gomoku()
+	game.loop()
